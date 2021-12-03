15c2c32448a802f2d849d2eb70c8775c6c65d633
Michał Kurkowski <michal.e.kurkowski@gmail.com>
2021-12-03 20:32:27 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                            // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("HelloWorld!"); // [3]
+        System.out.println("michal");
+        System.out.println("32");
+        System.out.println("k");
+    }
+}
---
+++ kodilla-intro/src/main/java/Nowa.java
@@ -0,0 +1,9 @@
+public class Nowa {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+    }
+}
