3dabe536dc3ede6e7d588e9fa7e7357382c14c4d
Michał Kurkowski <michal.e.kurkowski@gmail.com>
2021-12-15 20:31:07 +0100

    Initial version

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OS.java
@@ -0,0 +1,12 @@
+package com.kodilla.inheritance.homework;
+
+public class OS2020 extends OperatingSystem {
+
+    public void showAnything() {
+        System.out.println("I am showing anything.");
+    }
+    @Override
+    public void turnOn() {
+        System.out.println("Turn on operating system");
+    }
+}
